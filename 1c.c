77caf81fcd59a4f60b2e7dce910158141acad517
vincent maina <mainavincent519@gmail.com>
2024-11-07 18:23:22 +0300

    Add files via upload

---
+++ 1c.c
@@ -0,0 +1,30 @@
+/*
+program to calculate the electric balance using user defined function
+author: Vincent Kang'eri 
+reg no:CT101/G/25083/24
+*/
+
+
+#include <stdio.h>
+
+int main(){
+
+int i,j;
+int marks[2][3]={{1,2,3},{4,5,6}};
+for(i=0;i<2;i++){
+for(j=0;j<3;j++){
+printf("marks [%d][%d]=%d\n",i,j,marks[i][j]);
+}
+}
+
+
+
+
+
+return 0;
+
+
+
+
+
+}
